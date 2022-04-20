9749fbb4e99ba3ec71ee7571dfe72e12f31d26d4
isaac-easie <74269684+isaac-easie@users.noreply.github.com>
2022-04-20 20:37:46 +0300

    Add files via upload

---
+++ ransomware.py
@@ -0,0 +1,104 @@
+true = ''
+while true == '':
+    import os
+    os.system('clear')
+    import time as t   
+    
+    #RANSIMWARE SECTION by figitif
+
+    tool_name = ('''█▀▀ █░█ █▀▀ █ ▀█▀ █▀▀ █▀▀ █░█ 
+█▀░ █▄█ █▄█ █ ░█░ ██▄ █▄▄ █▀█ 
+
+'''+' 𓂀 𝒯𝒽𝒾𝓈 𝓉𝑜𝑜𝓁 𝓌𝒶𝓈 𝒸𝓇𝑒𝒶𝓉𝑒𝒹 𝒷𝓎 𝒻𝓊𝑔𝒾-𝓉𝑒𝒸𝒽 𓂀')
+    import getpass
+    print ('\n'+tool_name)
+    os.system('date')
+    t.sleep(1)
+    print ('\nThe tools password is☞ 0000'+'\n')
+    print ('Enter the tools password ')
+    t.sleep(1)
+    tool_pw = getpass.getpass('password :'.upper())
+    t.sleep(1)
+    print('checking.....###')
+    t.sleep(2)
+    os.system('clear')
+    t.sleep(1)
+   
+
+    while tool_pw == '0000':
+         print('tool is unlocked ')
+         t.sleep(1)
+         os.system('clear')
+         tool_list = input(tool_name+'''\n
+
+ [::] Select An Attack For Your Victim [::]
+
+[01] Facebook      [11] Twitch       [21] DeviantArt
+[02] Instagram     [12] Pinterest    [22] Badoo
+[03] Google        [13] Snapchat     [23] Origin
+[04] Microsoft     [14] Linkedin     [24] DropBox
+[05] Netflix       [15] Ebay         [25] Yahoo
+[06] Paypal        [16] Quora        [26] Wordpress
+[07] Steam         [17] Protonmail   [27] Yandex
+[08] Twitter       [18] Spotify      [28] StackoverFlow
+[09] Playstation   [19] Reddit       [29] Vk
+[10] Tiktok        [20] Adobe        [30] XBOX
+[31] Mediafire     [32] Gitlab       [33] Github
+[34] Discord
+
+[99] About         [00] Exit'''+'\n'+'Choose options >>  ')
+      
+         lock_notes = ('''YOUR FILES HAS BEEN LOCED! \nYour documents, photos, databases and other important files have been locked with strongest encryption and unique key, generated for this android. Private decryption key is stored on a secret Internet server and nobody can decrypt your files until you pay and obtain the private key for the tool administrators.\nThe server will eliminate the key after 24h  of which even the admins will lose access to it''')      
+         t.sleep(1)
+         os.system('clear')
+         print(tool_name)
+         os.system('date')
+        #print(tool_list)
+         t.sleep(1)
+         print('\nThis tool is for educational purposes only')
+         t.sleep(1)
+         os.system('clear')
+         print(tool_list)
+         t.sleep(0.5)
+         print(' \nencrypting your data in 0,5 secs!!,,,,##')
+         t.sleep(0.25)
+         print ('\n#.....15%')
+         t.sleep(0.25)
+         print('##########.....100%'+'\nData encrypted successfully')
+         t.sleep(3)
+         os.system('clear')
+         contact = input(tool_name+'\n'+lock_notes+'\n'+'\nEnter (y) to contact the developers on WhatsApp inorder to get your files decrypted'+'\n'+'\n>>   ')
+         os.system('date')
+         t.sleep(2)
+         if contact.upper() == 'Y':
+               os.system('clear')
+               print(tool_name)
+               os.system('date')
+               admins = int(input ('\nWhich developer would you like to contact (1/2)'+'\n1>>Isaac-Easie'+'\n2>>Figitif'+'\n☞  '))
+               if admins == 1:
+                   os.system('clear')
+                   print(tool_name)
+                   os.system('date')
+                   os.system('clear')
+                   print('contacting isaac-easie...#')
+                   t.sleep(2)
+                   print('opening WhatsApp...')
+                   t.sleep(1)
+                   os.system("xdg-open https://wa.me/+237650920378")
+               if admins == 2:
+                     os.system('clear')
+                     print(tool_name)
+                     print('\ncontacting Figitif...#')
+                     t.sleep(2)
+                     print('opening WhatsApp...')
+                     t.sleep(1)
+                     os.system("xdg-open https://wa.me/+256702344675")
+    else:
+        os.system('clear')
+        print(tool_name)
+        os.system('date')
+        print('\nincorrect password'+'\nThe correct tool password is☞ 0000')
+        t.sleep(3)
+   
+
+
